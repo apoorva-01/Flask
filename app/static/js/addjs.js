420fecfd24871150b080407634b603a1bb18b5d3
Punerva Singh <79405229+PunervaSingh@users.noreply.github.com>
2021-06-08 13:04:22 +0530

    Update addjs.js

--- app/static/js/addjs.js
+++ app/static/js/addjs.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $("input[type='radio']").change(function() {
-        if ($(this).val() === "yes") {
+        if ($(this).val() == "yes") {
             $("#ifYes").show(800);
         } else {
             $("#ifYes").hide(800);
